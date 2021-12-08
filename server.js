cc884eddca7220f373505a6e5b486368cf3becdf
Dario Bnz <developfsa@gmail.com>
2021-12-08 11:47:06 -0300

    express validator y que se yo

--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const usersRoutes = require('./routes/api/users')
+const authRoutes = require('./routes/api/auth')
+const profileRoutes = require('./routes/api/profile')
+const postsRoutes = require('./routes/api/posts')
+
+const auth = require('./middlewares/auth')
+
+const connectDB = require('./config/db')
+
+//iniciamos el mware
+app.use(express.json({extended : false}))
+
+//conectamos a db
+connectDB()
 //test
-app.get('/test', (req, res) => res.send('BE R-E activa') )
+app.get('/test', (req, res) => res.send('BEnd api rest activo') )
+
+//routes
+
+app.use('/api/users', usersRoutes)
+app.use('/api/posts', postsRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/profile',  profileRoutes)
 
 app.listen(PORT , () => {
    console.log(`servidor iniciado en el puerto: ${PORT}`)
