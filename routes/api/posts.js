d610298c1269360612837806ae32b5711e2b3ac2
Dario Bnz <developfsa@gmail.com>
2021-12-25 11:30:02 -0300

    added files

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,6 @@ const Profile = require('../../models/Profile')
 const Post = require('../../models/Post')
 
 
-
 //@route POST api/posts
 //desc: create post
 //@private
@@ -48,5 +47,50 @@ router.post('/', [ auth, [
     }
 
 })
+
+//@route GET api/posts
+//desc: get all posts
+//@public?? Private??
+//private:=>router.get('/', auth ,async (req, res) => {
+ 
+router.get('/', async (req, res) => {
+    try {
+        
+        const posts = await Post.find().sort({date: -1})
+
+        res.json(posts)
+
+    } catch (error) {
+
+        console.error(err.message)
+        
+        res.status(500).send('Server Error')
+    }
+})
+
+//@route GET api/posts/:id
+//desc: get post by Id
+//@Private
+ 
+    router.get('/:id', auth ,async (req, res) => {
+        try {
+            
+            const post = await Post.findById(req.params.id)
+            
+            if(!post) return res.status(404).json({msg: 'La publicación no existe'}) 
+
+            res.json(post)
+    
+        } catch (err) {
+            
+            console.error(err.message)
+            
+            if(err.kind === 'ObjectId'){
+                return res.status(404).json({msg: 'La publicacion no existe'})
+            }
+
+            res.status(500).send('Server Error')
+        }
+    })
 
 module.exports = router
