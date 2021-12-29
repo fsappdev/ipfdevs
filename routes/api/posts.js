c36a6483928baca04b0738d456f893cd4e652a5f
Dario Bnz <developfsa@gmail.com>
2021-12-29 13:04:43 -0300

    backend ready, just rest delete all posts from profile

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const { check, validationResult } = require('express-validator')
 const auth = require('../../middlewares/auth')
 const User = require('../../models/User')
-const Profile = require('../../models/Profile')
 const Post = require('../../models/Post')
+const { cookie } = require('request')
 
 
 //@route POST api/posts
@@ -193,5 +193,95 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 
 })
+
+//@route PUT api/posts/comment/:id
+//desc: comment on posts
+//@private
+router.post('/comment/:id', [ auth, [
+    check('text','text is required')
+    .not()
+    .isEmpty()
+]], async (req, res) => {
+    
+    const errors = validationResult(req)
+    
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array})
+    }
+
+    try {
+        
+        const user = await User.findById(req.user.id).select('-password')
+
+        const post = await Post.findById(req.params.id)
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        }
+
+        post.comments.unshift(newComment) 
+
+        await post.save()
+
+        res.json(post.comments)
+
+    } catch (err) {
+
+        console.error(err.message)
+        
+        res.status(500).send('Server Error')
+    }
+
+})
+
+
+//@route DELETE api/posts/comment/:id/:comment_id
+//desc: delete a comment on posts
+//@private
+router.delete('/comment/:id/:comment_id', auth ,async (req, res) => {
+    
+    try {
+
+        console.log('user-id:=>',req.user.id)
+        const post = await Post.findById(req.params.id)
+        
+        if(!post) return res.status(404).json({msg:'post not found'})
+
+        console.log('si hay post ?=>',post)
+        
+        //pull out commen
+        const comment = await post.comments.find(comment => comment.id === req.params.comment_id)
+
+        if(!comment) return res.status(404).json({msg: 'el comentario no existe'})
+
+        //check users: only post owner & comment owner can delete comments.
+        if(comment.user.toString() !== req.user.id && 
+            post.user.toString() !== req.user.id) { 
+            return res.status(401).json({msg: 'no tiene autorización'})
+        } 
+
+        // const removeIndex = post.likes.map(like => like.user.toString() === req.user.id).indexOf() 
+        //get remove index
+        const removeIndex = post.comments
+            .map(comment => comment.user.toString() === req.user.id).indexOf()    
+
+        post.comments.splice(removeIndex, 1)
+
+        await post.save()
+
+        return res.json(post.comments)
+
+    } catch (err) {
+
+        console.error(err.message)
+        
+        return res.status(500).send('Server Error')
+    }
+
+})
+
 
 module.exports = router
